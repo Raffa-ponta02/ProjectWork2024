47e9af57c64eafe83fd2ba29d6813d9d5e75b510
Raffaele Pontarini <raffaele.ponta@gmail.com>
2024-02-01 12:52:43 +0100

    Creazione di gith e del main
    aggiunta la cartella creata in precedenza

---
+++ Angular/1Pw0.0/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './Frontend/main-page/main-page.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
